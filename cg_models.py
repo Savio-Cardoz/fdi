344eb576d0f73bfdc0d6cbd50e01d0f5259c491a
faisal khan <faisalkhan@faisals-MacBook-Air.local>
2017-06-28 14:13:25 +0530

    Adding models

---
+++ cg_models.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+class State(object):
+    """docstring for State"""
+
+    def __init__(self, name, code):
+        super(State, self).__init__()
+        self.name = name  # name of the state
+        self.code = code  # code in the state machine
+        self.transitions = []  # Transitions of the state. This would include objects of class Transition
+
+    def __str__(self):
+        return self.name
+
+
+# This class is used to depict a transition from ione state to another
+class Transition(object):
+    """docstring for Transition"""
+
+    def __init__(self, name, trs_to, trs_from, condition):
+        super(Transition, self).__init__()
+        self.name = name  # name of the transition
+        self.trs_to = trs_to  # name of the destination state
+        self.trs_from = trs_from  # name of the source state
+        self.condition = condition  # condition of transition
+
+    def __str__(self):
+        return self.name
+
